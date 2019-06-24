2875aa49b2f352c64f08d0231339c954a9e188da
Ryan Foster <ryan@openidauthority.com>
2019-06-24 17:11:35 -0400

    Create starter project

--- OAuthNotes/Controllers/HomeController.cs
+++ OAuthNotes/Controllers/HomeController.cs
@@ -1,37 +1,13 @@
-﻿using System.Collections.Generic;
-using Microsoft.AspNetCore.Authorization;
-using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Mvc;
 
 namespace OAuthNotes.Controllers
 {
-    [Authorize]
     public class HomeController : Controller
     {
-        private static List<string> _notes;
-
-        public HomeController()
-        {
-            if (_notes == null)
-            {
-                _notes = new List<string> {"", "", ""};
-            }
-        }
-
         public IActionResult Index()
         {
 
-            return View(_notes);
-        }
-
-        [HttpPost]
-        public IActionResult Add(string note)
-        {
-            _notes.Add(note);
-            if (_notes.Count > 3)
-            {
-                _notes.RemoveAt(0);
-            }
-            return RedirectToAction("Index");
+            return View();
         }
     }
 }
--- OAuthNotes/Startup.cs
+++ OAuthNotes/Startup.cs
@@ -26,68 +26,6 @@ namespace OAuthNotes
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddAuthentication(options =>
-            {
-                // If an authentication cookie is present, use it to get authentication information
-                options.DefaultAuthenticateScheme = CookieAuthenticationDefaults.AuthenticationScheme;
-                options.DefaultSignInScheme = CookieAuthenticationDefaults.AuthenticationScheme;
-
-                // If authentication is required, and no cookie is present, use Okta (configured below) to sign in
-                options.DefaultChallengeScheme = "Okta";
-            })
-            .AddCookie() // cookie authentication middleware first
-            .AddOAuth("Okta", options =>
-            {
-                // Oauth authentication middleware is second
-
-                var oktaDomain = Configuration.GetValue<string>("Okta:OktaDomain");
-
-                // When a user needs to sign in, the will be redirected to the authorize endpoint
-                options.AuthorizationEndpoint = $"{oktaDomain}/oauth2/default/v1/authorize";
-
-                // Okta's OAuth server is OpenID compliant, so request the standard openid
-                // scopes when redirecting to the authorization endpoint
-                options.Scope.Add("openid");
-                options.Scope.Add("profile");
-                options.Scope.Add("email");
-
-                // Afterthe user signs in, an authorization code will be sent to a callback
-                // in this app. The OAuth middleware will intercept it
-                options.CallbackPath = new PathString("/authorization-code/callback");
-
-                // The OAuth middleware will send the ClientId, ClientSecret, and the
-                // authorization code to the token endpoint, and get an access token in return
-                options.ClientId = Configuration.GetValue<string>("Okta:ClientId");
-                options.ClientSecret = Configuration.GetValue<string>("Okta:ClientSecret");
-                options.TokenEndpoint = $"{oktaDomain}/oauth2/default/v1/token";
-
-                // Below we call the userinfo endpoint to get information about the user
-                options.UserInformationEndpoint = $"{oktaDomain}/oauth2/default/v1/userinfo";
-                
-                // Describe how to map the user info we receive to user claims
-                options.ClaimActions.MapJsonKey(ClaimTypes.NameIdentifier, "sub");
-                options.ClaimActions.MapJsonKey(ClaimTypes.Name, "given_name");
-                options.ClaimActions.MapJsonKey(ClaimTypes.Email, "email");
-
-                options.Events = new OAuthEvents
-                {
-                    OnCreatingTicket = async context =>
-                    {
-                        // Get user info from the userinfo endpoint and use it to populate user claims
-                        var request = new HttpRequestMessage(HttpMethod.Get, context.Options.UserInformationEndpoint);
-                        request.Headers.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
-                        request.Headers.Authorization = new AuthenticationHeaderValue("Bearer", context.AccessToken);
-
-                        var response = await context.Backchannel.SendAsync(request, HttpCompletionOption.ResponseHeadersRead, context.HttpContext.RequestAborted);
-                        response.EnsureSuccessStatusCode();
-
-                        var user = JObject.Parse(await response.Content.ReadAsStringAsync());
-
-                        context.RunClaimActions(user);
-                    }
-                };
-            });
-
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
         }
 
@@ -107,8 +45,6 @@ namespace OAuthNotes
 
             app.UseHttpsRedirection();
             app.UseStaticFiles();
-
-            app.UseAuthentication();
 
             app.UseMvc(routes =>
             {
